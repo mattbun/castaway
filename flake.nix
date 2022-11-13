3313486c2da2b39b9a0f1ecc44eea394b3d453cd
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2022-11-13 00:08:18 -0500

    chore(nix): add flake-compat

--- flake.nix
+++ flake.nix
@@ -1,4 +1,9 @@
 {
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
+
   inputs.dream2nix.url = "github:nix-community/dream2nix";
   outputs = inp:
     inp.dream2nix.lib.makeFlakeOutputs {
